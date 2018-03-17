b9637b1a5a6080dbeed41a08d73c2e6ad1bc1f27
Jihin Ip <jim_ip20@hotmail.com>
2018-03-17 23:35:07 +0000

    Fizzbuzz completed rspec and test file

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,40 @@
+require 'fizzbuzz'
+
+describe 'fizzbuzz' do
+  it 'returns "1" when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+  
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+    
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
+  
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+  
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
+  
+  it 'returns "14" when passed 14' do
+    expect(fizzbuzz(14)).to eq 14
+  end
+  
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  
+  it 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
+  
+end
